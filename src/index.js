dcdd91cea9997de9a0fb7afe29251dbff1b18dea
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-13 01:07:42 +0400

    console.log(state);

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('index.js');
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1;
+        default:
+            return state;
+    }
+}
+
+let state = reducer(undefined, { type: 'INC' });
+console.log(state);
+state = reducer(state, { type: 'INC' });
+console.log(state);
