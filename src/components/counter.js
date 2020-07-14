076285e8ade7ed1169ed5a035dabe69314d46e63
ArmenMesropyan <mesropyan200315@gmail.com>
2020-07-14 13:41:01 +0400

    Used react-redux connect function to map state and dispatch to props and used redux bindACtionCreators to bind actions

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import * as actions from '../actions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
 const Counter = ({ counter, inc, dec, rnd }) => (
     <>
         <h1>Simple Counter with Redux!</h1>
         <span id="counter">{ counter || 0 }</span>
-        <button onClick={inc}>DEC</button>
-        <button onClick={dec}>INC</button>
+        <button onClick={dec}>DEC</button>
+        <button onClick={inc}>INC</button>
         <button onClick={rnd}>RND</button>
     </>
 );
 
-export default Counter;
+const mapStateToProps = (state) => {
+    return {
+        counter: state,
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(actions, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
